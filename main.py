9bbfa9f5def02ccb663693cdd0fcea9eeb1956b9
chigaevi <chigaev.i@gmail.com>
2022-08-07 19:23:18 +0700

    add_func_if_same_intelligence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-from pprint import pprint
 
 def get_all_superhero_list (url='https://akabab.github.io/superhero-api/api/all.json'):
     superhero_powerstat = requests.get(url)
@@ -8,13 +7,17 @@ def get_all_superhero_list (url='https://akabab.github.io/superhero-api/api/all.
 def smartes_superhero(list_heros):
     all_superheros = get_all_superhero_list()
     max_intelligence = 0
+    smartest_hero = []
     for name in list_heros:
         for superhero in all_superheros:
             if superhero['name'] == name:
                 intelligence = superhero['powerstats']['intelligence']
                 if intelligence > max_intelligence:
                     max_intelligence = intelligence
-                    smartest_hero = name
+                    smartest_hero = [name]
+                elif intelligence == max_intelligence:
+                    max_intelligence = intelligence
+                    smartest_hero.append(name)
     return print(smartest_hero)
 
 def get_name_intelligence_list():
@@ -23,8 +26,8 @@ def get_name_intelligence_list():
         print(superhero['name'],'-',superhero['powerstats']['intelligence'])
 
 if __name__ == '__main__':
-    superheros = ['Hulk', 'Captain America', 'Thanos','Watcher']
+    # superheros = ['Hulk', 'Captain America', 'Thanos']
+    superheros = ['Hulk', 'Captain America', 'Thanos', 'Watcher', 'Vision']
     smartes_superhero(superheros)
     # get_name_intelligence_list()
-
 
