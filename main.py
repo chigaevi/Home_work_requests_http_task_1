84917bbaf855b938e60ec66e4f7708eb8c2856fd
chigaevi <chigaev.i@gmail.com>
2022-08-07 19:14:11 +0700

    add_func_get_all_superhero_list/smartes_superhero/get_name_intelligence_list

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+import requests
+from pprint import pprint
 
+def get_all_superhero_list (url='https://akabab.github.io/superhero-api/api/all.json'):
+    superhero_powerstat = requests.get(url)
+    return superhero_powerstat.json()
 
+def smartes_superhero(list_heros):
+    all_superheros = get_all_superhero_list()
+    max_intelligence = 0
+    for name in list_heros:
+        for superhero in all_superheros:
+            if superhero['name'] == name:
+                intelligence = superhero['powerstats']['intelligence']
+                if intelligence > max_intelligence:
+                    max_intelligence = intelligence
+                    smartest_hero = name
+    return print(smartest_hero)
+
+def get_name_intelligence_list():
+    all_superheros = get_all_superhero_list()
+    for superhero in all_superheros:
+        print(superhero['name'],'-',superhero['powerstats']['intelligence'])
 
 if __name__ == '__main__':
-
+    superheros = ['Hulk', 'Captain America', 'Thanos','Watcher']
+    smartes_superhero(superheros)
+    # get_name_intelligence_list()
 
 
